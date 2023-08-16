61e6b476b94cf0bf577b4d9f79fd3315791c42cb
fowlk1kd@gmail.com <fowlk1kd@gmail.com>
2023-08-16 23:53:02 +0000

    Created ServerModule1, Form1.ProjectForm and Form1.ItemTemplate1; Edited settings and Form1

--- client_code/Form1/ItemTemplate1/__init__.py
+++ client_code/Form1/ItemTemplate1/__init__.py
@@ -1,30 +1,14 @@
-from ._anvil_designer import Form1Template
-
+from ._anvil_designer import ItemTemplate1Template
 from anvil import *
 import anvil.server
+import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-import anvil.users
 
-
-
-class Form1(Form1Template):
-
+class ItemTemplate1(ItemTemplate1Template):
   def __init__(self, **properties):
-
     # Set Form properties and Data Bindings.
-
     self.init_components(**properties)
-
-    # Continue until user logs in
-    while not anvil.users.login_with_form():
-      pass
-
-    self.my_projects = anvil.server.call('get_projects')
-
-    #for row in self.my_projects.search():
-    #  pd = 
-
 
     # Any code you write here will run before the form opens.
